70763d0d158d75c834b65fd12e5cc240acb9e951
NANXI_GSGS <281206814@qq.com>
2019-01-16 17:49:00 +0800

    2019.01.16 网络完成，针对案例二还需继续完善

--- BackPropagation.py
+++ BackPropagation.py
@@ -16,30 +16,30 @@ class Ann(object):
 
     '''
     神经元结构
-    输入神经元
     隐藏层权重
     输出层权重
     输入层截距
     隐藏层截距
+    激活函数
 
     '''
-    def __init__(self,neurons__structure,
+    def __init__(self,neurons_structure,
                  hidden_layer_weight=None,
                  output_layer_weight=None,
-                 hidden_layer_bias=None,
-                 output_layer_bias=None,
+                 hidden_layer_bias=random.random(),
+                 output_layer_bias=random.random(),
                  active_method='sigmoid'):
 
-        self.input_layer_neurons_num = neurons__structure[0]
+        self.input_layer_neurons_num = neurons_structure[0]
         self.active_method = active_method
 
-        self.hidden_layer_neurons = []
-        self.hidden_layer_neurons_num = neurons__structure[1]
+        self.hidden_layer_neurons = [0] * neurons_structure[1]
+        self.hidden_layer_neurons_num = neurons_structure[1]
         self.hidden_layer_weight = hidden_layer_weight
         self.hidden_layer_bias = hidden_layer_bias
 
-        self.output_layer_neurons = []
-        self.output_layer_neurons_num = neurons__structure[2]
+        self.output_layer_neurons = [0] * neurons_structure[2]
+        self.output_layer_neurons_num = neurons_structure[2]
         self.output_layer_weight = output_layer_weight
         self.output_layer_bias = output_layer_bias
 
@@ -69,31 +69,33 @@ class Ann(object):
                     self.output_layer_weight[i] = random.random()
 
     def train(self,input_layer_neurons,target):
-        self.forward_propagation(input_layer_neurons)
 
-        self.back_propagation(target)
+        self.forward_propagation(input_layer_neurons)
+        total_error, output_errors = self.output_error(target)
+        self.back_propagation(target, input_layer_neurons)
+        return total_error
 
     # 前向传播
     def forward_propagation(self,input_layer_neurons):
         for i in range(self.hidden_layer_neurons_num):
-            self.hidden_layer_neurons.append(input_layer_neurons[0] * self.hidden_layer_weight[i*2]
-                                             + input_layer_neurons[1] * self.hidden_layer_weight[i*2+1]
-                                             + 1 * self.hidden_layer_bias)
+            hidden_layer_neurons = 0
+            for j in range(self.input_layer_neurons_num):
+                hidden_layer_neurons += input_layer_neurons[j] * self.hidden_layer_weight[(i * self.input_layer_neurons_num) + j]
+            self.hidden_layer_neurons[i] = hidden_layer_neurons + 1 * self.hidden_layer_bias
 
         for i in range(len(self.hidden_layer_neurons)):
             self.hidden_layer_neurons[i] = self.active_function(self.hidden_layer_neurons[i])
 
         for i in range(self.output_layer_neurons_num):
-            self.output_layer_neurons.append(self.hidden_layer_neurons[0] * self.output_layer_weight[i*2]
-                                             + self.hidden_layer_neurons[1] * self.output_layer_weight[i*2+1]
-                                             + 1 * self.output_layer_bias)
+            output_layer_neurons = 0
+            for j in range(self.hidden_layer_neurons_num):
+                output_layer_neurons += self.hidden_layer_neurons[j] * self.output_layer_weight[(i * self.output_layer_neurons_num) + j]
+            self.output_layer_neurons[i] = output_layer_neurons + 1 * self.output_layer_bias
 
         for i in range(len(self.output_layer_neurons)):
             self.output_layer_neurons[i] = self.active_function(self.output_layer_neurons[i])
 
-    def back_propagation(self,target):
-        total_errors = self.output_error(target)
-        print(total_errors)
+    def back_propagation(self,target,input_layer_neurons):
         pd_total_2_output_weights = []
         pd_total_2_hidden_weights = []
 
@@ -106,11 +108,29 @@ class Ann(object):
                 pd_total_2_output_weights.append(pd_1 * pd_2 * pd_3)
 
         # 求总体误差对输出层神经元的偏导(总体误差分开)
-        for i in range(self.output_layer_neurons_num):
-            pd_total_2_hidden = []
-            for   ？？？？？？？？？？
+        pd_total_2_hidden = []
+        for i in range(self.hidden_layer_neurons_num):
+            pd_output_2_hidden = 0
+            for j in range(self.output_layer_neurons_num):
+                pd_1 = self.pd_total_error_2_out_out(target[j],self.output_layer_neurons[j])
+                pd_2 = self.pd_out_out_2_out_net(self.output_layer_neurons[j])
+                pd_output_2_hidden += pd_1 * pd_2 * self.output_layer_weight[self.hidden_layer_neurons_num * j + i]
+            pd_total_2_hidden.append(pd_output_2_hidden)
 
-        print(pd_total_2_output_weights)
+        # 最后求隐藏神经元对隐藏层权重的偏导并与总体对输出层偏导相乘
+        for i in range(self.hidden_layer_neurons_num):
+            pd_1 = self.pd_out_out_2_out_net(self.hidden_layer_neurons[i])
+            for j in range(self.input_layer_neurons_num):
+                pd_total_2_hidden_weights.append(pd_1 * input_layer_neurons[j] * pd_total_2_hidden[i])
+
+        self.weight_update(pd_total_2_output_weights,pd_total_2_hidden_weights)
+
+    def weight_update(self,output_weight_update_coefficient,hidden_weight_update_coefficient):
+        for i in range(len(self.output_layer_weight)):
+            self.output_layer_weight[i] -= output_weight_update_coefficient[i] * self.LEARNING_RATE
+
+        for i in range(len(self.hidden_layer_weight)):
+            self.hidden_layer_weight[i] -= hidden_weight_update_coefficient[i] * self.LEARNING_RATE
 
     def active_function(self,input_number):
         if self.active_method == 'sigmoid':
@@ -119,10 +139,14 @@ class Ann(object):
             return None
 
     def output_error(self,target_output):
-        total_errors = 0
+        total_error = 0
+        output_errors = []
         for i in range(self.output_layer_neurons_num):
-            total_errors += 1 / 2 * (target_output[i] - self.output_layer_neurons[i]) ** 2
-        return total_errors
+            output_error = 1 / 2 * (target_output[i] - self.output_layer_neurons[i]) ** 2
+            output_errors.append(output_error)
+            total_error += output_error
+        # print('output_layer_neurons --->',self.output_layer_neurons)
+        return total_error,output_errors
 
     # 用误差对权重系数求偏导，得到权重系数对整体误差的影响程度，(链式法则)
     # 一：求整体误差(target)对输出(out_out)的偏导
@@ -138,6 +162,25 @@ class Ann(object):
     def pd_out_net_2_weight(self,index):
         return self.hidden_layer_neurons[index]
 
+
 if __name__ == '__main__':
-    Ann_test = Ann((2,2,2),(0.15,0.20,0.25,0.30),(0.40,0.45,0.50,0.55),0.35,0.60)
-    Ann_test.train((0.05,0.10),(0.01,0.99))
+    # 测试一：
+    # Ann_test = Ann((2,2,2),(0.15,0.20,0.25,0.30),(0.40,0.45,0.50,0.55),0.35,0.60)
+    # for i in range(10000):
+    #     total_error = Ann_test.train((0.05, 0.10), (0.01, 0.99))
+    #     print('trains count --->', i, '  total_error ---->', total_error)
+    # print(Ann_test.output_layer_neurons)
+
+    # 测试二：
+    Ann_test = Ann((2, 5, 1))
+    training_sets = [
+        [[0, 0], [0]],
+        [[0, 1], [1]],
+        [[1, 0], [1]],
+        [[1, 1], [0]]
+    ]
+    for i in range(10000):
+        training_inputs, training_outputs = [[0, 0], [0]]
+        total_error = Ann_test.train(training_inputs, training_outputs)
+        print('trains count --->', i, '  total_error ---->', total_error)
+    print(Ann_test.hidden_layer_weight)
